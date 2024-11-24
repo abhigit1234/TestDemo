ad808170f60fa615687a63cf4bd97240e9bf6589
abhilash <abhilashnaidu21@gmail.com>
2024-11-24 19:20:29 +0530

    new

--- TestDemo/src/test/java/TestRunner/Runner.java
+++ TestDemo/src/test/java/TestRunner/Runner.java
@@ -22,7 +22,32 @@ public class Runner extends AbstractTestNGCucumberTests {
 	public Object[][] scenarios(){
 		return super.scenarios();
 	}
+}
+	/*
+	 * @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value
+	 * = "false")
+	 * 
+	 * @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value =
+	 * "pretty,json:Reports/one.json,html:Reports/two.html,junit:Reports/three.xml")
+	 * 
+	 * @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "")
+	 * 
+	 * @ConfigurationParameter(key =
+	 * Constants.EXECUTION_MODE_FEATURE_PROPERTY_NAME,value = "concurrent")
+	 * 
+	 * @ConfigurationParameter(key =
+	 * Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME,value = "true")
+	 * //------------This is compulsory for parallel scenarios----------------
+	 * 
+	 * @ConfigurationParameter(key =
+	 * Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME,value = "fixed")
+	 * 
+	 * @ConfigurationParameter(key =
+	 * Constants.PARALLEL_CONFIG_FIXED_MAX_POOL_SIZE_PROPERTY_NAME,value = "2")
+	 * 
+	 * @ConfigurationParameter(key =
+	 * Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME,value = "2")
+	 */
 
 	
-	
-}
+
